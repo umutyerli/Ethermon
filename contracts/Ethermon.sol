fdc25ee19d6c6a6efd28cfb48c4cb95e027fc509
Umut Yerli <umutyerli@Mittys-MacBook.local>
2021-06-04 15:37:27 -0500

    init commit

---
+++ contracts/Ethermon.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract Ethermon is ERC721 {
+    constructor() ERC721("Ethermon", "EMON") {
+
+    }
+}
